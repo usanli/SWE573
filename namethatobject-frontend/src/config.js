e4f0f996e8b63ce536bd22fe7c894450a42d5a06
Umut Şanlı <umut.sanli@ayabilisim.com>
2024-11-06 02:13:35 +0300

    -environment files for frontend added

--- namethatobject-frontend/src/config.js
+++ namethatobject-frontend/src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-export const API_BASE_URL = 'http://127.0.0.1:8000'; // Adjust based on your setup
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL; // Adjust based on your setup
 export const MYSTERIES_ENDPOINT = `${API_BASE_URL}/posts/`; // Backend endpoint path
